31a1a7e65d67eb32d5b490a057bafb076d45f660
LINGALA RAJESH <136982328+Rajesh2607@users.noreply.github.com>
2025-04-25 10:51:01 +0530

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        backgroundImage: {
-          'gradient-to-b': 'linear-gradient(to bottom, var(--tw-gradient-stops))',
-        },
         sans: ['Inter', 'sans-serif'], // or 'Poppins', etc.
       },
     },
   },
-  plugins: [
-    require('tailwind-scrollbar'),
-    require('tailwind-scrollbar-hide'),
-    
-  ],
+  plugins: [],
 };
